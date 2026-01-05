cfaa49734c60136a56a5f0c1ecc1a27440a08ac6
Furmissile <squirrelmissile.mail@gmail.com>
2026-01-05 15:12:38 -0500

    expose DataModel class

--- scurrypy/core/__init__.py
+++ scurrypy/core/__init__.py
@@ -1,12 +1,14 @@
 # scurrypy/core
 
 from .error import DiscordError
+from .model import DataModel
 from .intents import Intents
 from .permissions import Permissions
 from .addon import Addon
 
 __all__ = [
     "Addon",
+    "DataModel",
     "DiscordError",
     "Intents",
     "Permissions"
